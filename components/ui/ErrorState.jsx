6cf4b0be8e0bd2f4402adef8c8df5ec08d2c9565
Sergio Torres Fernández <sergiopaulodev@gmail.com>
2026-01-26 10:47:41 -0300

    refactor(ui): make ErrorState aware of error semantic types

--- components/ui/ErrorState.jsx
+++ components/ui/ErrorState.jsx
@@ -1,8 +1,17 @@
 import { UI_ERROR_MESSAGES } from "@/lib/ui/error-messages";
+import { ERROR_TYPES } from "@/lib/errors/error-types";
 
-export function ErrorState({ code }) {
-    const message = UI_ERROR_MESSAGES[code] ?? "Unexpected error";
-    
+export function ErrorState({ code, type }) {
+    let message = UI_ERROR_MESSAGES[code];
+
+    if (!message) {
+        if (type === ERROR_TYPES.USER) {
+            message = "We could't find what you were looking for.";
+        } else {
+            message = "Something went wrong. Please try again later.";
+        }
+    }  
+
     return (
         <div className="error-state">
             <p>{message}</p>
